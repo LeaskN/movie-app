1e39c28e78c165214f0efd764a0e371d1ebba5cf
Nicholas <leask.nicholas@gmail.com>
2021-05-18 22:40:49 -0400

    successful api request

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -6,6 +6,12 @@ class MovieCard extends Component {
         this.state={}
     }
 
+    componentDidMount(){
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=fed5b7c4a0fea83e14866a8dd8cb6baa&language=en-US&page=1')
+            .then(res => res.json())
+            .then(res => console.log(res))
+    }
+
     render(){
         return( 
             <h1>Test</h1>
