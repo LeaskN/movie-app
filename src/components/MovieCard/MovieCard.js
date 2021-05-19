a0366c741dda7e424308f33b6bc9d9a18da5e3b7
Nicholas <leask.nicholas@gmail.com>
2021-05-18 23:22:11 -0400

    home renders list of movies, styled movie cards

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './MovieCard.css'
 
 class MovieCard extends Component {
     constructor(props){
@@ -6,15 +7,11 @@ class MovieCard extends Component {
         this.state={}
     }
 
-    componentDidMount(){
-        fetch('https://api.themoviedb.org/3/movie/popular?api_key=fed5b7c4a0fea83e14866a8dd8cb6baa&language=en-US&page=1')
-            .then(res => res.json())
-            .then(res => console.log(res))
-    }
-
     render(){
         return( 
-            <h1>Test</h1>
+            <div className="movieCard">
+                <h1>{this.props.original_title}</h1>
+            </div>
         )
     }
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
+import React, { Component } from 'react';
 import MovieCard from '../components/MovieCard/MovieCard'
 
-function Home() {
-  return (
-    <MovieCard />
-  );
+class Home extends Component {
+    constructor(props){
+      super(props)
+      this.state={}
+    }
+
+    componentDidMount(){
+      fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=fed5b7c4a0fea83e14866a8dd8cb6baa&language=en-US&page=1')
+        .then(res => res.json())
+        .then(res => this.setState({movies: res}))
+    }
+
+    render(){
+      return( 
+        this.state.movies ?
+        this.state.movies.results.map(movie => <MovieCard {...movie}/>) : <h1>No movies</h1>
+      )
+    }
 }
 
 export default Home;
