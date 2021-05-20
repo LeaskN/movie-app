4d5c388ac6fe3ec0f5075dc36b3aaf9d2c710433
Nicholas <leask.nicholas@gmail.com>
2021-05-19 22:39:56 -0400

    added date filter

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -4,27 +4,38 @@ import './MovieCard.css'
 class MovieCard extends Component {
     constructor(props){
         super(props)
-        this.state={}
+        this.state={range:0}
     }
 
     componentDidMount(){
         fetch(`https://api.themoviedb.org/3/movie/${this.props.id}?api_key=fed5b7c4a0fea83e14866a8dd8cb6baa&language=en-US&append_to_response=release_dates`)
             .then(res => res.json())
             .then(res => this.setState({movieData: res}))
-    }
+    }    
 
     render(){
-        if(this.state.movieData){
+        if(
+            (this.state.movieData && 
+                (this.props.minRange === undefined || this.props.maxRange === undefined)
+            ) || (this.state.movieData && 
+                (
+                    (new Date(this.state.movieData.release_date).getTime() > this.props.minRange) && 
+                    (new Date(this.state.movieData.release_date).getTime() < this.props.maxRange)
+                )
+            )
+        ){
             const { original_title, release_date, vote_average, poster_path, runtime, genres } = this.state.movieData;
             let certification = this.state.movieData.release_dates.results.filter(rel_date => rel_date.iso_3166_1 === 'US' )[0].release_dates[0].certification;
             return( 
-                <div className="movieCard">
-                    <h1>{original_title}({release_date.slice(0, 4)})</h1>
-                    <h1>Average Rating: {vote_average}</h1>
-                    <h1>{certification.length > 0 ? certification : 'N/A'}</h1>
-                    <h1>Runtime: {runtime}</h1>
-                    <img src={`https://image.tmdb.org/t/p/original/${poster_path}`} />
-                    <h1>Genre: {genres.map((genre, i) => genres.length - 1 !== i ? genre.name +', ' : genre.name)}</h1>
+                <div>
+                    <div className="movieCard">
+                        <h1>{original_title}({release_date.slice(0, 4)})</h1>
+                        <h1>Average Rating: {vote_average}</h1>
+                        <h1>{certification.length > 0 ? certification : 'N/A'}</h1>
+                        <h1>Runtime: {runtime}</h1>
+                        <img alt="movie-poster" src={`https://image.tmdb.org/t/p/original/${poster_path}`} />
+                        <h1>Genre: {genres.map((genre, i) => genres.length - 1 !== i ? genre.name +', ' : genre.name)}</h1>
+                    </div>
                 </div>
             )
         }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,19 +4,31 @@ import MovieCard from '../components/MovieCard/MovieCard'
 class Home extends Component {
     constructor(props){
       super(props)
-      this.state={}
+      this.state={
+        minRange: undefined,
+        maxRange: undefined,
+      }
     }
 
     componentDidMount(){
-      fetch('https://api.themoviedb.org/3/movie/top_rated?api_key=fed5b7c4a0fea83e14866a8dd8cb6baa&language=en-US&page=1&language=en-US')
+      fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=fed5b7c4a0fea83e14866a8dd8cb6baa&language=en-US&page=${this.state.page}&language=en-US`)
         .then(res => res.json())
         .then(res => this.setState({movies: res}))
     }
 
+    inputChange(e){
+      this.setState({[e.target.id]: new Date(e.target.value).getTime()})
+    }
+
+
     render(){
       return( 
-        this.state.movies ?
-        this.state.movies.results.map(movie => <MovieCard id={movie.id}/>) : <h1>No movies</h1>
+        <div>
+          <p>Start Date:<input type="date" min="1" id="minRange" onChange={event => this.inputChange(event)}></input></p>
+          <p>End Date: <input type="date" min={this.state.range} id="maxRange" onChange={event => this.inputChange(event)}></input></p>
+          {this.state.movies ?
+          this.state.movies.results.map(movie => <MovieCard key={movie.id} id={movie.id} minRange={this.state.minRange} maxRange={this.state.maxRange}/>) : <h1>No movies</h1>}
+        </div>
       )
     }
 }
