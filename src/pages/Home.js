97e982f58e1c27abfe09724a931d170cd9ac4d0d
Nicholas <leask.nicholas@gmail.com>
2021-05-20 00:14:36 -0400

    added interval to prevent multiple page loads

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
+import { isCompositeComponent } from 'react-dom/test-utils';
 import MovieCard from '../components/MovieCard/MovieCard'
 
 class Home extends Component {
     constructor(props){
       super(props)
       this.state={
+        didScroll: false,
         movies: [],
         minRange: undefined,
         maxRange: undefined,
-        page: 1
+        page: 1,
+        currentCount: 3,
       }
       this.fetchNextPage = this.fetchNextPage.bind(this);
-      this.pageEnd = this.pageEnd.bind(this);
+      this.infiniteScroll = this.infiniteScroll.bind(this);
+      this.allowScroll = this.allowScroll.bind(this);
     }
 
     componentDidMount(){
+      window.addEventListener('scroll', this.infiniteScroll);
       this.fetchNextPage()
     }
 
     fetchNextPage(){
-      console.log(this.state.page)
-
       fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=fed5b7c4a0fea83e14866a8dd8cb6baa&language=en-US&page=${this.state.page}&language=en-US`)
         .then(res => res.json())
         .then(res => {console.log(res); return res.results})
@@ -28,25 +31,35 @@ class Home extends Component {
     }
 
     inputChange(e){
-      this.setState({[e.target.id]: new Date(e.target.value).getTime()})
+      this.setState({[e.target.id]: new Date(e.target.value).getTime()});
     }
 
-    pageEnd(){
-      if(window.scrollY > document.body.clientHeight - 2000){
-        let currentPage = this.state.page;
-        this.setState({page: currentPage + 1})
+    infiniteScroll() {
+      if (
+        window.innerHeight + document.documentElement.scrollTop > document.documentElement.offsetHeight && this.state.didScroll === false
+      ) {
+        this.setState({didScroll: true});
+        let newPage = this.state.page;
+        newPage++
+        this.setState({ page: newPage });
         this.fetchNextPage();
+        this.allowScroll();
       }
     }
 
+    allowScroll(){
+      setInterval(() => {
+        this.setState({didScroll: false})
+      }, 250)
+    }
     render(){
       
       return( 
-        <div onWheel={this.pageEnd}>
+        <div>
           <p>Start Date:<input type="date" min="1" id="minRange" onChange={event => this.inputChange(event)}></input></p>
           <p>End Date: <input type="date" min={this.state.range} id="maxRange" onChange={event => this.inputChange(event)}></input></p>
           {this.state.movies ?
-          this.state.movies.map(movie => <MovieCard key={movie.id} id={movie.id} minRange={this.state.minRange} maxRange={this.state.maxRange}/>) : <h1>No movies</h1>}
+          this.state.movies.map(movie => <MovieCard key={movie.id + movie.title} id={movie.id} minRange={this.state.minRange} maxRange={this.state.maxRange}/>) : <h1>No movies</h1>}
         </div>
       )
     }
